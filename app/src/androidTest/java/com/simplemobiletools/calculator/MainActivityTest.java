00c95f642d969112043d88af821fe8ef8a7b96b3
Kevin Zhang <izkevin52@gmail.com>
2018-02-12 15:10:30 -0500

    fix unused import

--- app/src/androidTest/java/com/simplemobiletools/calculator/MainActivityTest.java
+++ app/src/androidTest/java/com/simplemobiletools/calculator/MainActivityTest.java
@@ -15,7 +15,6 @@ import static android.support.test.espresso.action.ViewActions.longClick;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static junit.framework.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
