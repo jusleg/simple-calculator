283a0c6fa24ddd4b875d72ee8535f87b9abf0ff0
Mathieu Leblanc <0mat.leb2@gmail.com>
2018-04-15 00:59:44 -0400

    Improve BaseActivity ABC

--- app/src/main/kotlin/com/simplemobiletools/calculator/activities/BaseActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/activities/BaseActivity.kt
@@ -3,6 +3,7 @@ package com.simplemobiletools.calculator.activities
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import com.simplemobiletools.calculator.R
 import com.simplemobiletools.calculator.helpers.BaseCalculatorImpl
 import com.simpletools.calculator.commons.activities.SimpleActivity
@@ -17,9 +18,7 @@ import com.simplemobiletools.commons.extensions.*
 
 class BaseActivity : SimpleActivity(), Calculator {
 
-    private var storedTextColor = 0
     private var vibrateOnButtonPress = true
-    private var storedUseEnglish = false
 
     private lateinit var currentBase: String
     private lateinit var calc: BaseCalculatorImpl
@@ -31,10 +30,22 @@ class BaseActivity : SimpleActivity(), Calculator {
 
         calc = BaseCalculatorImpl(this, applicationContext)
         currentBase = DEC
-        getButtonIds().forEach {
+
+        setNumberButtonListeners()
+        setOperationButtonListeners()
+        setLongClickListeners()
+
+        updateViewColors(base_holder, config.textColor)
+        btn_dec.setTextColor(config.customPrimaryColor)
+    }
+
+    private fun setNumberButtonListeners() {
+        arrayOf(btn_0, btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9).forEach {
             it.setOnClickListener { calc.numpadClicked(it.id); checkHaptic(it) }
         }
+    }
 
+    private fun setOperationButtonListeners() {
         btn_dec.setOnClickListener { setBaseToDecimal(); checkHaptic(it) }
         btn_oct.setOnClickListener { setBaseToOctal(); checkHaptic(it) }
         btn_bin.setOnClickListener { setBaseToBinary(); checkHaptic(it) }
@@ -45,12 +56,12 @@ class BaseActivity : SimpleActivity(), Calculator {
         btn_inv.setOnClickListener { calc.handleOperation(INV); checkHaptic(it) }
         btn_equals.setOnClickListener { calc.handleEquals(); checkHaptic(it) }
         btn_clear.setOnClickListener { calc.handleClear(); checkHaptic(it) }
+    }
+
+    private fun setLongClickListeners() {
         btn_clear.setOnLongClickListener { calc.handleReset(); true }
         formula.setOnLongClickListener { copyToClipboard(false) }
         result.setOnLongClickListener { copyToClipboard(true) }
-
-        updateViewColors(base_holder, config.textColor)
-        btn_dec.setTextColor(config.customPrimaryColor)
     }
 
     private fun checkHaptic(view: View) {
@@ -64,13 +75,14 @@ class BaseActivity : SimpleActivity(), Calculator {
         currentBase = DEC
         btn_dec.setTextColor(config.customPrimaryColor)
         btn_dec.isEnabled = false
-
         btn_oct.setTextColor(config.textColor)
         btn_oct.isEnabled = true
-
         btn_bin.setTextColor(config.textColor)
         btn_bin.isEnabled = true
+        toggleDecimalNumberButtons()
+    }
 
+    private fun toggleDecimalNumberButtons() {
         btn_2.isEnabled = true
         btn_3.isEnabled = true
         btn_4.isEnabled = true
@@ -86,13 +98,14 @@ class BaseActivity : SimpleActivity(), Calculator {
         currentBase = OCT
         btn_dec.setTextColor(config.textColor)
         btn_dec.isEnabled = true
-
         btn_oct.setTextColor(config.customPrimaryColor)
         btn_oct.isEnabled = false
-
         btn_bin.setTextColor(config.textColor)
         btn_bin.isEnabled = true
+        toggleOctalNumberButtons()
+    }
 
+    private fun toggleOctalNumberButtons() {
         btn_2.isEnabled = true
         btn_3.isEnabled = true
         btn_4.isEnabled = true
@@ -103,18 +116,20 @@ class BaseActivity : SimpleActivity(), Calculator {
         btn_9.isEnabled = false
     }
 
+
     private fun setBaseToBinary() {
         calc.convertToBinary(currentBase)
         currentBase = BIN
         btn_dec.setTextColor(config.textColor)
         btn_dec.isEnabled = true
-
         btn_oct.setTextColor(config.textColor)
         btn_oct.isEnabled = true
-
         btn_bin.setTextColor(config.customPrimaryColor)
         btn_bin.isEnabled = false
+        toggleBinaryNumberButtons()
+    }
 
+    private fun toggleBinaryNumberButtons() {
         btn_2.isEnabled = false
         btn_3.isEnabled = false
         btn_4.isEnabled = false
@@ -124,8 +139,6 @@ class BaseActivity : SimpleActivity(), Calculator {
         btn_8.isEnabled = false
         btn_9.isEnabled = false
     }
-
-    private fun getButtonIds() = arrayOf(btn_0, btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9)
 
     private fun copyToClipboard(copyResult: Boolean): Boolean {
         var value = formula.value
